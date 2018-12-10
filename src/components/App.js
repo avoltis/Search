dd8e187639110a1c7efe0154550c77a9f7c46f7c
Voltis <v.agolli@live.com>
2018-12-10 22:11:31 +0100

    axios lib get

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
     onSearchSubmit(term) {
-        console.log(term);
+
+        axios.get('https://api.unsplash.com/search/photos', {
+            params: { query: term },
+            headers: {
+                Authorization: 'Client-ID 9797f7eadb370d1676b63ac6a4f85d4e088e3b4996c98e4c56a53b3ba49f58f9'
+            }
+        });
     }
 
     render() {
