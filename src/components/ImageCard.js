a0128fb31f8bff53b9b6961a0abd53cb50d1f7a9
Voltis <v.agolli@live.com>
2018-12-11 16:34:56 +0100

    image list/card css fixes

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -2,12 +2,33 @@ import React from 'react';
 import './ImageList.css';
 
 class ImageCard extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { spans: 0 }
+
+        this.imageRef = React.createRef();
+    }
+
+    componentDidMount() {
+        this.imageRef.current.addEventListener('load', this.setSpans);
+    }
+
+    setSpans = () => {
+        const height = this.imageRef.current.clientHeight;
+
+        const spans = Math.ceil(height / 10);
+
+        this.setState({ spans })
+    }
+
     render() {
-        const {description, urls} = this.props.image;
+        const { description, urls } = this.props.image;
 
         return (
-            <div className="zoom">
+            <div className="zoom" style={{ gridRowEnd: `span ${this.state.spans}` }}>
                 <img
+                    ref={this.imageRef}
                     alt={description}
                     src={urls.regular}
                 />
