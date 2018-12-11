1d8b211c749133436a00a4f303647ca25a2c3a9a
Voltis <v.agolli@live.com>
2018-12-11 15:46:22 +0100

    updated name

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import unsplash from '../api/unsplash';
 import SearchBar from './SearchBar';
-import ImageList from './ImageList';
+import ImageList from './ImageList.js';
 
 class App extends React.Component {
     state = { images: [] };
