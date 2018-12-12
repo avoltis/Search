8e1dd5b8ce91b133cb426246466079027e12ad27
Voltis <v.agolli@live.com>
2018-12-12 12:18:51 +0100

    videoDetail component

--- src/components/App.js
+++ src/components/App.js
@@ -4,12 +4,14 @@ import youtube from '../api/youtube';
 import SearchBar from './SearchBar/SearchBar';
 import ImageList from './ImageList/imageList';
 import VideoList from './VideoList/VideoList';
+import VideoDetail from './VideoDetail/VideoDetail';
 
 class App extends React.Component {
     state = {
-        term : '',
+        term: '',
         images: [],
         videos: [],
+        selectedVideo: null
     };
 
     onSearchSubmit = (searchData) => {
@@ -34,7 +36,7 @@ class App extends React.Component {
             }
         });
 
-        this.setState({term, images: response.data.results, videos: [] });
+        this.setState({ term, images: response.data.results, videos: [] });
     }
 
     getVideos = async (term) => {
@@ -47,14 +49,25 @@ class App extends React.Component {
         this.setState({ term, images: [], videos: response.data.items });
     }
 
+    onVideoSelect = (video) => {
+        this.setState({ selectedVideo: video });
+    }
+
     render() {
         let layout = ''
-        if (this.state.images.length > 0)
+        if (this.state.images.length > 0) {
             layout = <ImageList images={this.state.images} />
-        else if (this.state.videos.length > 0)
-            layout = <VideoList videos={this.state.videos} />
-        else if (this.state.term || '')
+        }
+        else if (this.state.videos.length > 0) {
+            layout =
+                <div>
+                    <VideoDetail video={this.state.selectedVideo} />
+                    <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos} />
+                </div>
+        }
+        else if (this.state.term || '') {
             layout = "Found 0 Result, Try with a different search!"
+        }
 
         return (
             <div className="ui container" style={{ marginTop: '10px' }}>
--- src/components/VideoList/VideoList.js
+++ src/components/VideoList/VideoList.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import VideoItem from '../VideoItem/VideoItem';
 
-const VideoList = ({ videos }) => {
+const VideoList = ({ videos, onVideoSelect }) => {
     const renderetList = videos.map(video => {
-        return <VideoItem video={video}/>
+        return <VideoItem onVideoSelect={onVideoSelect} video={video} />
     });
 
     return <div className="ui relaxed divided list">{renderetList}</div>
